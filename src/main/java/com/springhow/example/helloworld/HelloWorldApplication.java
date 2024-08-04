313be86dfbdaa9e6035fee9a198d36069091fc2c
Sohail130 <84300969+Sohail130@users.noreply.github.com>
2024-08-04 10:43:45 +0530

    Update HelloWorldApplication.java

--- src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
+++ src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
@@ -23,7 +23,7 @@ public class HelloWorldApplication extends SpringBootServletInitializer {
 
     @RequestMapping("/")
     String helloWorld() {
-        return "Hello World!";
+        return "Deploying War file on Test and Prod";
     }
 
 }
