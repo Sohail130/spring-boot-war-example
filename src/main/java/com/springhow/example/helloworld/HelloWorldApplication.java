0ce805d52ca2504cdb05755419b90ea9eb2cd5d2
Sohail130 <84300969+Sohail130@users.noreply.github.com>
2024-12-13 04:51:39 +0530

    Update HelloWorldApplication.java

--- src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
+++ src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
@@ -23,7 +23,7 @@ public class HelloWorldApplication extends SpringBootServletInitializer {
 
     @RequestMapping("/")
     String helloWorld() {
-        return "Deploying War file on Test and Prod server using Jenkinsfile";
+        return "Deploying War file on Test and Prod server using Jenkinsfile Pipeline";
     }
 
 }
